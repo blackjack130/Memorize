ddb635634f765a47fd99bb98011921ebb453b106
Chris <chris.jackson159@gmail.com>
2023-10-18 17:21:20 -0400

    Set up initial foundation for cards / flipping actions

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -7,15 +7,50 @@
 
 import SwiftUI
 
+//View that will hold literally everything. Instantiate CardView here by using ForEach loop
 struct ContentView: View {
+	let emojis = ["🤣","🤷🏾‍♂️","🥰","🥳", "😌", "😘", "🥶", "🫥", "🤗", "🤠", "🤥", "🤧"]
+	
 	var body: some View {
 		VStack {
-			Image(systemName: "globe")
-					.imageScale(.large)
-					.foregroundStyle(.tint)
-			Text("Hello, world!")
+			ScrollView {
+				cards
 			}
+			Spacer()
+		}
 		.padding()
+	}
+	
+	var cards: some View {
+		LazyVGrid(columns: [GridItem(.adaptive(minimum: 65)), GridItem(.adaptive(minimum: 65)), GridItem(.adaptive(minimum: 65)), GridItem(.adaptive(minimum: 65)), GridItem(.adaptive(minimum: 65))]) {
+			ForEach(0..<emojis.count, id:\.self) { index in
+				CardView(content: emojis[index])
+					.aspectRatio(2/3, contentMode: .fit)
+			}
+		}
+		.foregroundColor(.red)
+		.padding()
+	}
+}
+
+struct CardView: View {
+	@State var isFaceUp = false
+	let content: String
+	
+	var body: some View {
+		ZStack {
+			let base = RoundedRectangle(cornerRadius: 12)
+			Group {
+				base.foregroundColor(.white)
+				base.strokeBorder(lineWidth: 2)
+				Text(content).font(.largeTitle)
+			}
+			.opacity(isFaceUp ? 1 : 0)
+			base.fill().opacity(isFaceUp ? 0 : 1)
+		}
+		.onTapGesture {
+			isFaceUp.toggle()
+		}
 	}
 }
 
