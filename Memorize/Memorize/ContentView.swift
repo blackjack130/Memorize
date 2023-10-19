beedae86093ea66f891a103e4e55264e3e2e619b
Chris <chris.jackson159@gmail.com>
2023-10-19 17:34:34 -0400

    Added buttons to change emoji themes, tidied up UI

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -7,15 +7,16 @@
 
 import SwiftUI
 
-//View that will hold literally everything. Instantiate CardView here by using ForEach loop
 struct ContentView: View {
-	let emojis = ["🤣","🤷🏾‍♂️","🥰","🥳", "😌", "😘", "🥶", "🫥", "🤗", "🤠", "🤥", "🤧"]
+	@State var activeEmojiSet: Array<String> = []
+	let vehicleEmojis = ["✈️","🚀","🚗","🚘","🚘","🚀","🛫","🚗","🛫","✈️"]
+	let techEmojis = ["📱","💻","⌨️","📱","📡","💻","⌨️","📡"]
+	let randomEmojis = ["🤣","🤷🏾‍♂️","🤷🏾‍♂️","🥳", "🤣", "😘", "🥶", "😘", "🥶", "🥳", "🤥", "🤥"]
 	
 	var body: some View {
 		VStack {
-			ScrollView {
-				cards
-			}
+			Text("Memorize!").font(.largeTitle)
+			cards
 			Spacer()
 			themeButtons
 		}
@@ -24,8 +25,8 @@ struct ContentView: View {
 	
 	var cards: some View {
 		LazyVGrid(columns: [GridItem(.adaptive(minimum: 65)), GridItem(.adaptive(minimum: 65)), GridItem(.adaptive(minimum: 65)), GridItem(.adaptive(minimum: 65)), GridItem(.adaptive(minimum: 65))]) {
-			ForEach(0..<emojis.count, id:\.self) { index in
-				CardView(content: emojis[index])
+			ForEach(0..<activeEmojiSet.count, id:\.self) { index in
+				CardView(content: activeEmojiSet[index])
 					.aspectRatio(2/3, contentMode: .fit)
 			}
 		}
@@ -33,43 +34,56 @@ struct ContentView: View {
 		.padding()
 	}
 	
-	func changeTheme(theme: Int, symbol: String) -> some View {
-		Button(action: {
-			//update emojis
-		}, label: {
+	func changeTheme(theme: Int, symbol: String, title: String) -> some View {
+		VStack {
+			Button(action: {
+				switch theme {
+				case 0 :
+					activeEmojiSet = vehicleEmojis
+				case 1 :
+					activeEmojiSet = techEmojis
+				case 2 :
+					activeEmojiSet = randomEmojis
+				default:
+					print("An error has occurred")
+				}
+				activeEmojiSet.shuffle()
+			}, label: {
 				Image(systemName: symbol)
-		})
+			})
+			Text(title)
+				.foregroundStyle(Color.accentColor)
+				.font(.footnote)
+		}
 	}
 	
-	var theme1: some View {
-		//FIXME: add actual system image for the symbol
-		changeTheme(theme: 0, symbol: "globe")
+	var vehicleTheme: some View {
+		changeTheme(theme: 0, symbol: "car", title: "Vehicles")
 	}
 	
-	var theme2: some View {
-		changeTheme(theme: 1, symbol: "globe")
+	var techTheme: some View {
+		changeTheme(theme: 1, symbol: "desktopcomputer", title: "Tech")
 	}
 	
-	var theme3: some View {
-		changeTheme(theme: 2, symbol: "globe")
+	var randomTheme: some View {
+		changeTheme(theme: 2, symbol: "shuffle", title: "Random")
 	}
 	
 	var themeButtons: some View {
-		HStack {
-			theme1
-			Spacer()
-			theme2
-			Spacer()
-			theme3
+		HStack(spacing: 50) {
+			vehicleTheme
+			techTheme
+			randomTheme
 		}
 		.imageScale(.large)
-		.font(.largeTitle)
+		.font(.title2)
 	}
 }
 
 struct CardView: View {
 	@State var isFaceUp = false
 	let content: String
+	
 	
 	var body: some View {
 		ZStack {
